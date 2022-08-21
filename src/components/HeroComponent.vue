8cfea464ff0095c4d58cfa733a998a4e649ca9b0
Bill Walker <falklian@gmail.com>
2022-08-21 13:12:51 -0500

    chore: make page responsive

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="flex flex-row h-screen">
-    <div class="flex w-1/3 place-items-center place-content-end pr-16">
+  <div class="flex flex-col lg:flex-row lg:h-screen">
+    <div class="flex lg:w-1/3 place-items-center place-content-center mt-16 lg:mt-0 lg:place-content-end lg:pr-16">
       <Transition
         enter-active-class="delay-300 duration-1000 ease-out"
         enter-from-class="translate-y-4 opacity-0"
         enter-to-class="translate-y-0 opacity-100"
       >
-        <RoundedImage class="grayscale transition transform w-full" v-show="state.show" :image="portrait" />
+        <RoundedImage class="grayscale transition transform w-48 lg:w-full" v-show="state.show" :image="portrait" />
       </Transition>
     </div>
-    <div class="flex relative w-2/3 place-items-center justify-items-center pl-16">
+    <div class="flex flex-col lg:flex-row lg:relative lg:w-2/3 place-items-center justify-items-center lg:pl-16">
       <Transition
         enter-active-class="delay-500 duration-1000 ease-out"
         enter-from-class="translate-y-4 opacity-0"
         enter-to-class="translate-y-0 opacity-100"
       >
-        <div class="flex-col w-full transition transform" v-show="state.show">
-          <h1 class="text-7xl font-light">Bill</h1>
-          <h1 class="text-emerald-600 text-7xl font-bold">Walker</h1>
-          <div class="flex justify-between">
-            <div class="text-xl pt-4">Developer | DevOps | IT | Manager</div>
-            <div class="flex gap-1">
+        <div class="flex-col text-center lg:text-left lg:w-full transition transform mt-8 lg:mt-0" v-show="state.show">
+          <h1 class="text-5xl lg:text-7xl font-light">Bill</h1>
+          <h1 class="text-emerald-600 text-5xl lg:text-7xl font-bold">Walker</h1>
+          <div class="flex flex-col lg:flex-row lg:justify-between">
+            <div class="text-base lg:text-xl mt-8 lg:mt-4">Developer | DevOps | IT | Manager</div>
+            <div class="flex place-items-center place-content-center gap-1 mt-8 lg:mt-0">
               <SocialBadge icon="github" url="https://github.com/Falklian" />
               <SocialBadge icon="linkedin-in" url="https://www.linkedin.com/in/billkwalker/" />
             </div>
@@ -33,22 +33,22 @@
         enter-to-class="translate-y-0 opacity-100"
       >
         <div
-          class="flex justify-between transition transform absolute border-t-2 border-gray-500 bottom-16 left-16 right-0 pt-4"
+          class="flex flex-wrap text-center md:text-left md:flex-nowrap w-11/12 lg:w-auto mt-8 lg:mt-0 justify-between transition transform lg:absolute border-t-2 border-gray-500 lg:bottom-16 lg:left-16 lg:right-0 pt-4"
           v-show="state.show"
         >
-          <div>
+          <div class="basis-full sm:basis-1/2 md:basis-auto">
             <p class="font-bold">Location</p>
             <p>Rock Island, IL</p>
           </div>
-          <div>
+          <div class="basis-full sm:basis-1/2 md:basis-auto mt-4 sm:mt-0">
             <p class="font-bold">Mobile</p>
             <p>309-749-5544</p>
           </div>
-          <div>
+          <div class="basis-full sm:basis-1/2 md:basis-auto mt-4 md:mt-0">
             <p class="font-bold">Website</p>
             <p><a href="https://billw.dev" class="hover:text-emerald-600">billw.dev</a></p>
           </div>
-          <div>
+          <div class="basis-full sm:basis-1/2 md:basis-auto mt-4 md:mt-0">
             <p class="font-bold">E-mail</p>
             <p><a href="mailto:me@billw.dev" class="hover:text-emerald-600">me@billw.dev</a></p>
           </div>
